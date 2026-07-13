930269c0a2126b22c5aab4ba20f466a59a381b30
xspeedHub0 <rogersolizfranco15@gmail.com>
2026-07-13 14:51:24 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'✅ Bot {bot.user} está online!')
+
+@bot.event
+async def on_message(message):
+    # Evita que el bot responda a sus propios mensajes
+    if message.author.id == bot.user.id:
+        return
+    
+    # Responde a TODOS los mensajes (usuarios y bots)
+    respuesta = "@everyone\nla tengo chica"
+    
+    await message.channel.send(respuesta)
+
+bot.run(os.getenv("DISCORD_TOKEN"))
